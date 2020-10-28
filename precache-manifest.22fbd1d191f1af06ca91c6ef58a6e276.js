c6d2df49e7152b610f6c2a676ebbd923741e6e94
John <johncraigmurphy@gmail.com>
2020-10-28 23:12:21 +0000

    deploy

---
+++ precache-manifest.22fbd1d191f1af06ca91c6ef58a6e276.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27a20327f718d08918aff1f990c30734",
+    "url": "/coronavirus-chart/index.html"
+  },
+  {
+    "revision": "150363c0e5128d8c3d53",
+    "url": "/coronavirus-chart/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bfcbc4dff15eb2745b47",
+    "url": "/coronavirus-chart/static/js/2.b7e5fd8c.chunk.js"
+  },
+  {
+    "revision": "bcec845f68886451796e4fb5926fad8e",
+    "url": "/coronavirus-chart/static/js/2.b7e5fd8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "150363c0e5128d8c3d53",
+    "url": "/coronavirus-chart/static/js/main.411eac3f.chunk.js"
+  },
+  {
+    "revision": "6c8863c42898f5de9fb5",
+    "url": "/coronavirus-chart/static/js/runtime-main.17b6d80e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coronavirus-chart/static/media/logo.5d5d9eef.svg"
+  }
+]);
