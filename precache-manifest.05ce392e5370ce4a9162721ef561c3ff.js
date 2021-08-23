68aeb3860a1c963e7a89091904affca47272d534
unknown <johncraigmurphy@gmail.com>
2021-08-23 14:58:08 +0100

    deploy

---
+++ precache-manifest.05ce392e5370ce4a9162721ef561c3ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8771a95d0c8313faf59ac66a3cf089bd",
+    "url": "/coronavirus-chart/index.html"
+  },
+  {
+    "revision": "a52f1b9124ddc2057a60",
+    "url": "/coronavirus-chart/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "0705b1b5757fc830cd8a",
+    "url": "/coronavirus-chart/static/js/2.2b26f8bf.chunk.js"
+  },
+  {
+    "revision": "361ebd21fae9f116f12a8724795ae423",
+    "url": "/coronavirus-chart/static/js/2.2b26f8bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a52f1b9124ddc2057a60",
+    "url": "/coronavirus-chart/static/js/main.604488e2.chunk.js"
+  },
+  {
+    "revision": "6c8863c42898f5de9fb5",
+    "url": "/coronavirus-chart/static/js/runtime-main.17b6d80e.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/coronavirus-chart/static/media/logo.ee7cd8ed.svg"
+  }
+]);
